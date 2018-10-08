740d610e94c0f2b0b4991ac1760ddd01ad1ff5f7
Juan Rodriguez <juan.rodriguez@etermax.com.uy>
2018-10-08 15:14:43 -0300

    Added base code algorithms

---
+++ Assets/Scripts/LevelGen/Chamber.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace LevelGen
+{
+    public class Chamber : MonoBehaviour
+    {
+        public Transform[] Exits;
+        public Collider Bounds;
+
+        private Level levelContainer;
+
+        void Start()
+        {
+            levelContainer = GetComponentInParent<Level>();
+            
+            if (levelContainer.LevelSize > 0)
+                GenerateAnnexes();
+        }
+
+        void GenerateAnnexes()
+        {
+            foreach (var e in Exits)
+            {
+                GenerateCorridor(e);
+            }
+        }
+        
+        void GenerateCorridor(Transform exit)
+        {
+            var candidate = Instantiate(levelContainer.Corridors.PickOne(), exit).GetComponent<Corridor>();
+            if (levelContainer.IsSectionValid(candidate.Bounds, Bounds))
+            {
+                candidate.transform.SetParent(levelContainer.transform);
+                levelContainer.RegisterNewSection(candidate.Bounds);
+            }
+            else
+            {
+                 Destroy(candidate.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LevelGen/Corridor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace LevelGen
+{
+    public class Corridor : MonoBehaviour
+    {
+        public Transform[] Exits;
+        public Collider Bounds;
+
+        private Level levelContainer;
+
+        void Start()
+        {
+            levelContainer = GetComponentInParent<Level>();
+            
+            if (levelContainer.LevelSize > 0)
+                GenerateAnnexes();
+        }
+
+        void GenerateAnnexes()
+        {
+            foreach (var e in Exits)
+            {
+                GenerateChamber(e);
+            }
+        }
+        
+        void GenerateChamber(Transform exit)
+        {
+            var candidate = Instantiate(levelContainer.Chambers.PickOne(), exit).GetComponent<Chamber>();
+            if (levelContainer.IsSectionValid(candidate.Bounds, Bounds))
+            {
+                candidate.transform.SetParent(levelContainer.transform);
+                levelContainer.RegisterNewSection(candidate.Bounds);
+            }
+            else
+            {
+                Destroy(candidate.gameObject);   
+            }
+        }
+    }
+}
